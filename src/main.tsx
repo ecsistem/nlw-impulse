dd09e0f1d328d0f96a86308cb5225c100682b207
Edson Costa <edson.13.10.2004@gmail.com>
2022-05-02 11:16:16 -0300

    Aula 1

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Home} from './pages/home'
+import './assets/styles/global/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import {Widget} from '././../../components/widget';
+
+export function Home() {
+
+  return (
+    <div>
+      <Widget />
+    </div>
+  )
+}
